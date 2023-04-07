60bfecf481c1fe1c960566222c28b6038bd00200
Bastien Goulet <bastien.goulet@uqar.ca>
2023-04-07 15:57:09 -0400

    Integration with Kafka topics as a pass through

--- service/Worker.cs
+++ service/Worker.cs
@@ -1,3 +1,5 @@
+using Confluent.Kafka;
+
 namespace INF36307.TP3;
 
 public class Worker : BackgroundService
@@ -11,10 +13,54 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        ConsumerConfig config = new ConsumerConfig
+        {
+            GroupId = "users-consumer-group",
+            BootstrapServers = "kafka-0.kafka.default.svc.cluster.local:9092,kafka-1.kafka.default.svc.cluster.local:9092,kafka-2.kafka.default.svc.cluster.local:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        ProducerConfig producerConfig = new ProducerConfig
+        {
+            BootstrapServers = "kafka-0.kafka.default.svc.cluster.local:9092,kafka-1.kafka.default.svc.cluster.local:9092,kafka-2.kafka.default.svc.cluster.local:9092",
+            BatchSize = 10000000,
+            LingerMs = 200,
+            CompressionType = CompressionType.Lz4,
+            Acks = Acks.All
+        };
+
+        using var consumerBuilder = new ConsumerBuilder<Ignore, string>(config).Build();
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+        consumerBuilder.Subscribe("users");
+
+        try
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+                
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var result = consumerBuilder.Consume(stoppingToken);
+                    Console.WriteLine($"Consumed message '{result.Message.Value}' at: '{result.TopicPartitionOffset}'.");
+                    
+                    producer.Produce(
+                        "notification", 
+                        new Message<Null, string> { Value=result.Message.Value },
+                        (deliveryReport) =>
+                        {
+                            Console.WriteLine($"Produced message '{deliveryReport.Message.Value}' at: '{deliveryReport.TopicPartitionOffset}'.");
+                        });
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumerBuilder.Close();
         }
     }
 }
